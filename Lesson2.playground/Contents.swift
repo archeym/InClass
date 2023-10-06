2b3678a202583f91c734c0f26ffd4d225bed5c05
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-10-06 13:19:37 +0200

    Updating lesson2

---
+++ Lesson2.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//MARK: -Lesson2
+//If else conditional statement
+
+
+var myNumber = 10
+
+if myNumber > 4 {
+    print("The 1 condition is true")
+}
+
+
+if myNumber < 4 && myNumber == 10 {
+    print("The 2 condition is true")
+} else if myNumber < 8 {
+    print("The 2 condition is true")
+} else {
+    print("Nothing here is true")
+}
